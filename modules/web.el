c9e4e7176fce5e8851425e5198f752a40a33ea95
Adithya Nair <adithyanair121@gmail.com>
2026-05-03 19:30:19 +0530

    i am become web developer (Astro config works)

---
+++ modules/web.el
@@ -0,0 +1,20 @@
+(use-package web-mode :ensure t)
+
+;; Astro Configuration
+(define-derived-mode astro-mode web-mode "astro")
+(setq auto-mode-alist
+      (append '((".*\\.astro\\'" . astro-mode))
+              auto-mode-alist))
+
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs
+               '(astro-mode . ("astro-ls" "--stdio"
+                               :initializationOptions
+                               (:typescript (:tsdk "./node_modules/typescript/lib")))))
+  :init
+  ;; auto start eglot for astro-mode
+  (add-hook 'astro-mode-hook 'eglot-ensure))
+
+(provide 'web)
